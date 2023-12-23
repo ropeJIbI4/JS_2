5095456f84bc40041bfd23daea5f52db5ecd4925
ropeJIbI4 <pogi199411@gmail.com>
2023-12-23 21:14:56 +0300

    first

---
+++ script.js
@@ -0,0 +1,30 @@
+let num1 = prompt();
+let num2 = prompt();
+if (num1 <= 1){
+    console.log(`num1 <= 1`)
+}
+else{
+    console.log(`num1 не выпоняет условие...`)
+};
+if (num2 >= 3){
+    console.log(`num2 >= 3`)
+}
+else{
+    console.log(`num2 не выпоняет условие...`)
+}
+
+
+let test = true;
+(test === true) ? console.log('+++'):console.log('---')
+
+let day = prompt("введите число от 0 до 31")
+
+if (day<=10){
+    console.log("первая декада")
+}
+else if (day>10,day<=20){
+    console.log("вторая декада")
+}
+else {console.log("третья декада")
+}
+
